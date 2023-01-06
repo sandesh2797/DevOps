07b89363f1f5d62d41784581d59f138005426c8f
sandesh2797 <70713518+sandesh2797@users.noreply.github.com>
2023-01-06 16:46:32 +0530

    Create task.sh

---
+++ task.sh
@@ -0,0 +1,17 @@
+export source="https://l"
+export key=2m4e
+
+ echo "create dir with dates"
+ for i in $
+ mkdir -p
+ d=$(date -r "$i" +%Y-%m-%d)
+ mv $i `date +"%Y%m%d"`
+done
+
+
+#for i in $source
+#do  dir=/`date +%Y-%m -r$source`
+    #mkdir -p $dir && mv $i $dir
+#done
+
+azcopy --destination "httckup/DATE/bagic/" --dest-keyJ2K8W
