c7e1a9639182a60ccb9910076699109286ea9029
sandesh2797 <70713518+sandesh2797@users.noreply.github.com>
2023-01-06 16:41:13 +0530

    find modified time

---
+++ find.sh
@@ -0,0 +1,4 @@
+find /var/log -type d -mtime +30 -exec rm -rf {} \;
+find $fpath -type d -mtime +10  -exec rm -rf {} \;
+find /opt/log/forever/heatsink/
+find /path/to/dir/ -type f -mtime +60 -delete
